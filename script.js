7bda41f5889d607d18e1b570867fa2037a36cc7e
GoodnessOno <ononogbugoodness@gmail.com>
2024-03-12 20:34:56 +0100

    POTRAIT COMMIT

--- script.js
+++ script.js
@@ -1,1 +1,4 @@
+const button = document.querySelector('#button');
+/* you use const when you're creating a variable that you do not want to change. Confirm the above code and how it relates to your html and css, also confirm the button in html and what it does */
 
+button.onclick = remainInPage /* confirm if this is correct */
