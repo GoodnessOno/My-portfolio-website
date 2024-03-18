097968a9b3192464e1b5436f9dcb32f1a85c3788
GoodnessOno <ononogbugoodness@gmail.com>
2024-03-18 17:41:25 +0100

    PHONE FIX CONTENT

--- script.js
+++ script.js
@@ -16,3 +16,30 @@ document.getElementsByClassName("form").addEventListener("submit", function(even
     xhr.setRequestHeader("Content-Type", "application/json");
     xhr.send(JSON.stringify(data));
 });
+// Get the HTML element that contains the website content
+const contentContainer = document.querySelector(".main_section");
+
+// Define a function to scroll the content container
+function scrollContent(direction) {
+  // Determine the new scroll position
+  let newScrollPos = contentContainer.scrollTop + direction;
+
+  // Make sure the new scroll position is within the bounds of the content
+  if (newScrollPos < 0) {
+    newScrollPos = 0;
+  } else if (newScrollPos > contentContainer.scrollHeight - window.innerHeight) {
+    newScrollPos = contentContainer.scrollHeight - window.innerHeight;
+  }
+
+  // Set the new scroll position
+  contentContainer.scrollTop = newScrollPos;
+}
+
+// Add event listeners for the up and down arrow keys
+document.addEventListener("keydown", (event) => {
+  if (event.key === "ArrowUp") {
+    scrollContent(-10);
+  } else if (event.key === "ArrowDown") {
+    scrollContent(10);
+  }
+});
